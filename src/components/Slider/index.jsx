fb21afdd4d8e7534475e2363f468c820b02b9ae0
Ciczau <123020783+Ciczau@users.noreply.github.com>
2023-02-28 00:25:55 +0100

    'final'

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -38,9 +38,9 @@ const ImageSlider = ({images, choose}) => {
             {images.map((image, index) => 
             <motion.div  style={{opacity: index === active ? "1" : "0", width: index === active ? "100%" : "0%", transition: "opacity ease-in-out 0.3s", zIndex: "2",display:"flex", justifyContent: "center", textAlign: "center"}}>
                 <img  src={image.slide} alt="main slide" key={index} style={{filter: "brightness(40%)", overflow: "hidden", maxHeight: "100vh", width: "100vw",objectFit: "cover", objectPosition: "top center"}}/>
-                <div style={{position: "absolute", zIndex: "3", color: "white", top: "40%",textShadow: "0px 0px 10px #000000d3",width: "50%",height: "auto",lineHeight: "3.5vw", fontSize: "4vw", display: "flex", justifyContent: "center", flexDirection: "column"}}>
+                <div style={{position: "absolute", zIndex: "3", color: "white", top: "40%",textShadow: "0px 0px 10px #000000d3",width: "50%",height: "auto",lineHeight: "3.0vw", fontSize: "3.5vw", display: "flex", justifyContent: "center", flexDirection: "column"}}>
                     <div>{image.text}</div>
-                    <a href="/offer" style={{fontSize: "2vw",marginTop: "5%",position: "relative",width: "50%",marginLeft: "25%",  backgroundColor: "#660101ff", padding: "2vw",borderRadius: "10px", boxShadow: "0px 0px 5px 3px #0000008f", color: "white"}}>KLIKNIJ TUTAJ</a>
+                    <a href="/offer" style={{fontSize: "1.5vw",marginTop: "5%",position: "relative",marginLeft: "15vw", marginRight: "15vw", backgroundColor: "#660101ff", padding: "1.3vw 0.5vw",borderRadius: "10px", boxShadow: "0px 0px 5px 3px #0000008f", color: "white"}}>KLIKNIJ TUTAJ</a>
                 </div>
                 <div style={{position: 'absolute', left: "0", bottom: "50%", zIndex: "3", marginLeft: "5vw",  display: "flex", cursor: "pointer"}} onClick={checkPrev}><FaAngleLeft size="3vw" color="white"/></div>
             <div style={{position: "absolute", right: "0", bottom: "50%",zIndex: "3", marginRight: "5vw",display: "flex", cursor: "pointer"}} onClick={checkNext}><FaAngleRight size="3vw" color="white"/></div>
@@ -48,7 +48,7 @@ const ImageSlider = ({images, choose}) => {
             )}
             <div style={{position: "absolute", zIndex: "3", justifyContent: "center", width: "100%",display: "flex", bottom: "20px"}}>
             {Array.from({length: images.length}).map((dot, index) => (
-                <div style={{width: "0.8vw", height: "0.8vw", borderRadius: "50%", boxShadow: index === active ? "0px 0px 5px 3px #0000008b" : "0px 0px 5px 3px #00000078", backgroundColor: index === active ?"#050505" : "#999898af", margin: "0 15px", }} onClick={() => setActive(index)}/>
+                <div style={{width: "0.8vw", height: "0.8vw", borderRadius: "50%", boxShadow: index === active ? "0px 0px 5px 3px #0000008b" : "0px 0px 5px 3px #00000078", backgroundColor: index === active ?"#fffbfb" : "#999898af", margin: "0 15px", }} onClick={() => setActive(index)}/>
             ))}
             </div>
         </div>
