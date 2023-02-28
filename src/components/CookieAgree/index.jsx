8e6d472ce32e373f59f2557e476f785a29ef9d35
Ciczau <123020783+Ciczau@users.noreply.github.com>
2023-02-28 02:26:10 +0100

    'final'

--- src/components/CookieAgree/index.jsx
+++ src/components/CookieAgree/index.jsx
@@ -1,14 +1,24 @@
 import React, { useState } from "react";
+import { useCookies } from "react-cookie";
 import * as S from './index.styles';
 
 const CookieAgree = () => {
     const [close, setClose] = useState(false);
     const [showMore, setShowMore] = useState(false);
+    const [cookie, setCookies] = useCookies(['cookieaccept']);
     const handleClick = () => {
         setShowMore(current => !current);
     }
+    const closeAlert = () => {
+        let date = new Date();
+        date.setTime(date.getTime() + 24 * 60 * 60 * 1000 * 365);
+        setCookies('cookieaccept', 'ok', {path: '/', expires: date });
+        setClose(true);
+    }
     return (
         <>
+        {!cookie.cookieaccept && (
+            <>
             {!close && (
             <S.Footer>
                 Korzystając ze strony zgadzasz się na używanie plików cookies <S.ReadMore   onMouseEnter={() => setShowMore(true)} onMouseLeave={() => setShowMore(false)} onClick={handleClick}>Czytaj więcej</S.ReadMore>
@@ -17,11 +27,13 @@ const CookieAgree = () => {
                         Pliki cookies na tej stronie są wykorzystywanie <br/>do przechowywanie danych na temat sesji logowania.<br/>
                     </S.MoreWrapper>
                 )}
-                <S.Button onClick={() => setClose(true)}>
+                <S.Button onClick={closeAlert}>
                     ZAMKNIJ
                 </S.Button>
             </S.Footer>
             )}
+            </>
+        )}
         </>
     )
 }
